db5268c8a97496fbdd1860797c992f3985e5ec7b
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2023-10-27 12:28:34 +0200

    Update rasppi55 (valve control)

--- rasppi55/socket_server.py
+++ rasppi55/socket_server.py
@@ -1,21 +1,15 @@
-# pylint: disable=R0913,W0142,C0103 
-
-import sys
-sys.path.insert(1, '/home/pi/PyExpLabSys')
+# pylint: disable=R0913,W0142,C0103
 import time
 import PyExpLabSys.common.valve_control as valve_control
-from PyExpLabSys.common.sockets import DateDataPullSocket
-from PyExpLabSys.common.sockets import DataPushSocket
+from PyExpLabSys.common.sockets import DateDataPullSocket, DataPushSocket
 
 valve_names = [0] * 20
 for i in range(0, 20):
     valve_names[i] = str(i + 1)
-Pullsocket = DateDataPullSocket('sniffer valvecontrol',
-                                valve_names, timeouts=[2]*20)
+Pullsocket = DateDataPullSocket('sniffer valvecontrol', valve_names, timeouts=[2] * 20)
 Pullsocket.start()
 
-Pushsocket = DataPushSocket('sniffer valvecontrol',
-                            action='enqueue')
+Pushsocket = DataPushSocket('sniffer valvecontrol', action='enqueue')
 Pushsocket.start()
 
 vc = valve_control.ValveControl(valve_names, Pullsocket, Pushsocket)
