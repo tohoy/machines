e7d97e0ca9d1fe5a0ed29e5cd9df4ff17e0d919f
Jakob Ejler Sørensen <lillebror.jakobes@gmail.com>
2024-10-03 11:32:58 +0200

    Improve host_status.py. last_accessed, cleanup, documentation

--- rasppi42/host_status.py
+++ rasppi42/host_status.py
@@ -1,6 +1,6 @@
-""" The program will check up status of a list of hosts 
+""" The program will check up on the status of a list of hosts 
 
-The following keys are saved in a attribute dictionary:
+The following keys are saved in an attribute dictionary:
   up:               How long the host has been on since last restart (days)
   load:             The CPU and IO utilization averaged over the last 15 minutes
   git_pyexplabsys:  Timestamp of last git commit (PyExpLabSys.git)
@@ -17,6 +17,19 @@ The following keys are saved in a attribute dictionary:
   ports:            All ports used by script
   location:         Location (´id´ in PURPOSE)
   purpose:          Short description (´purpose´ in PURPOSE)
+  last_accessed:    Last time an active socket or OS check via SSH was successful
+
+credentials.py module utilizes the following variables and functions:
+  sql_host (str):   SQL server hosname
+  db (str):         Database name in SQL server
+  user (str):       Username of host_checker user
+  passwd (str):     Password of host_checker user
+  get_user_and_password(hostname) (def):
+                    Function that returns a tuble (USER, PASSWORD) from a single
+                    hostname
+  suffix_list (list):
+                    Optional list of suffixes to append to the hostname besides
+                    the ones implied from the network configuration
 """
 import subprocess
 import datetime
@@ -24,6 +37,7 @@ import telnetlib
 import socket
 import threading
 import time
+
 try:
     import Queue as queue
 except ImportError:
@@ -34,14 +48,24 @@ from PyExpLabSys.common.system_status import SystemStatus
 from PyExpLabSys.common.utilities import get_logger
 import credentials
 
-default_user = credentials.host['default_user']
-default_passwd = credentials.host['default_passwd']
+try:
+    SUFFIX_LIST = credentials.suffix_list
+except AttributeError:
+    SUFFIX_LIST = []
 
 SELF = socket.gethostname()
-LOGGER = get_logger('Host Checker', level='debug', file_log=True,
-                    file_name='host_checker.log', terminal_log=True,
-                    email_on_warnings=False, email_on_errors=False,
-                    file_max_bytes=104857, file_backup_count=5)
+LOGGER = get_logger(
+    'Host Checker',
+    level='debug',
+    file_log=True,
+    file_name='host_checker.log',
+    terminal_log=True,
+    email_on_warnings=False,
+    email_on_errors=False,
+    file_max_bytes=104857,
+    file_backup_count=5,
+)
+
 
 def host_status(hostname, port):
     """ Report if a host is available on the network """
@@ -52,7 +76,7 @@ def host_status(hostname, port):
             subprocess.check_output(["ping", "-c1", "-W1", hostname])
         except subprocess.CalledProcessError:
             host_is_up = False
-    if port == 3389: # RDP
+    if port == 3389:  # RDP
         try:
             _ = telnetlib.Telnet(hostname, 3389)
         except socket.gaierror:
@@ -61,7 +85,8 @@ def host_status(hostname, port):
             host_is_up = False
     return host_is_up
 
-def uptime(hostname, port, username=default_user, password=default_passwd):
+
+def uptime(hostname, port, username, password):
     """ Fetch as much information as possible from a host """
     return_value = {}
     return_value['up'] = ''
@@ -72,19 +97,30 @@ def uptime(hostname, port, username=default_user, password=default_passwd):
     return_value['python_version'] = ''
     return_value['model'] = ''
     return_value['os_version'] = ''
+    return_value['last_accessed'] = ''
     if hostname == SELF:
         status = SystemStatus()
-        return_value['up'] = str(int(int(status.uptime()['uptime_sec']) / (60*60*24)))
+        return_value['up'] = str(
+            int(int(status.uptime()['uptime_sec']) / (60 * 60 * 24))
+        )
         return_value['load'] = status.load_average()['15m']
-        apt_up_time= status.last_apt_cache_change_unixtime()
-        return_value['apt_up'] = datetime.datetime.fromtimestamp(apt_up_time).strftime('%Y-%m-%d')
+        apt_up_time = status.last_apt_cache_change_unixtime()
+        return_value['apt_up'] = datetime.datetime.fromtimestamp(apt_up_time).strftime(
+            '%Y-%m-%d'
+        )
         gittime = status.last_git_fetch_unixtime()
         try:
-            return_value['git_pyexplabsys'] = datetime.datetime.fromtimestamp(gittime['PyExpLabSys']).strftime('%Y-%m-%d')
-            return_value['git_machines'] = datetime.datetime.fromtimestamp(gittime['machines']).strftime('%Y-%m-%d')
+            return_value['git_pyexplabsys'] = datetime.datetime.fromtimestamp(
+                gittime['PyExpLabSys']
+            ).strftime('%Y-%m-%d')
+            return_value['git_machines'] = datetime.datetime.fromtimestamp(
+                gittime['machines']
+            ).strftime('%Y-%m-%d')
         except TypeError:
             # Only for legacy purposes
-            return_value['git_pyexplabsys'] = datetime.datetime.fromtimestamp(gittime).strftime('%Y-%m-%d')
+            return_value['git_pyexplabsys'] = datetime.datetime.fromtimestamp(
+                gittime
+            ).strftime('%Y-%m-%d')
             return_value['git_machines'] = None
         return_value['host_temperature'] = status.rpi_temperature()
         return_value['python_version'] = status.python_version()
@@ -94,20 +130,27 @@ def uptime(hostname, port, username=default_user, password=default_passwd):
 
     # Specifically use SSH
     if port == 22:
-        uptime_string = subprocess.check_output(["sshpass",
-                                                 "-p",
-                                                 password,
-                                                 "ssh",
-                                                 '-o LogLevel=quiet',
-                                                 '-oUserKnownHostsFile=/dev/null',
-                                                 '-oStrictHostKeyChecking=no',
-                                                 username + "@" + hostname,
-                                                 'cat /proc/uptime /proc/loadavg'])
+        uptime_string = subprocess.check_output(
+            [
+                "sshpass",
+                "-p",
+                password,
+                "ssh",
+                '-o LogLevel=quiet',
+                '-oUserKnownHostsFile=/dev/null',
+                '-oStrictHostKeyChecking=no',
+                username + "@" + hostname,
+                'cat /proc/uptime /proc/loadavg',
+            ]
+        )
         uptime_raw = uptime_string.split('\n')[0]
-        uptime_value = str(int(float(uptime_raw.split()[0]) / (60*60*24)))
+        uptime_value = str(int(float(uptime_raw.split()[0]) / (60 * 60 * 24)))
         load = uptime_string.split('\n')[1].split()[2]
         return_value['up'] = uptime_value
         return_value['load'] = load
+        return_value['last_accessed'] = datetime.datetime.now().strftime(
+            '%Y-%m-%d %H:%M'
+        )
 
     if not port in [22, 3389]:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -119,13 +162,18 @@ def uptime(hostname, port, username=default_user, password=default_passwd):
             status = json.loads(received.decode())
             system_status = status['system_status']
             socket_status = status['socket_server_status']
-            uptime_value = str(int(int(system_status['uptime']['uptime_sec']) / (60*60*24)))
+            uptime_value = str(
+                int(int(system_status['uptime']['uptime_sec']) / (60 * 60 * 24))
+            )
             LOGGER.debug(uptime_value)
             load = str(system_status['load_average']['15m'])
             return_value['up'] = uptime_value
             return_value['load'] = load
             return_value['ports'] = json.dumps([key for key in socket_status.keys()])
             return_value['ports'] = [key for key in socket_status.keys()]
+            return_value['last_accessed'] = datetime.datetime.now().strftime(
+                '%Y-%m-%d %H:%M'
+            )
         except:
             return_value['up'] = 'Down'
             return_value['load'] = 'Down'
@@ -148,17 +196,19 @@ def uptime(hostname, port, username=default_user, password=default_passwd):
             ret = False
         if not ret:
             try:
-                os_string = subprocess.check_output([
-                    "sshpass",
-                    "-p",
-                    password,
-                    "ssh",
-                    '-o LogLevel=quiet',
-                    '-oUserKnownHostsFile=/dev/null',
-                    '-oStrictHostKeyChecking=no',
-                    username + "@" + hostname,
-                    'cat /etc/os-release',
-                ])
+                os_string = subprocess.check_output(
+                    [
+                        "sshpass",
+                        "-p",
+                        password,
+                        "ssh",
+                        '-o LogLevel=quiet',
+                        '-oUserKnownHostsFile=/dev/null',
+                        '-oStrictHostKeyChecking=no',
+                        username + "@" + hostname,
+                        'cat /etc/os-release',
+                    ]
+                )
                 os_version = os_string.decode().split('\n')
                 for os_line in os_version:
                     if os_line.startswith('VERSION='):
@@ -194,22 +244,29 @@ def uptime(hostname, port, username=default_user, password=default_passwd):
         try:
             gittime = system_status['last_git_fetch_unixtime']
             try:
-                git['PyExpLabSys'] = datetime.datetime.fromtimestamp(gittime['PyExpLabSys']).strftime('%Y-%m-%d')
-                git['machines'] = datetime.datetime.fromtimestamp(gittime['machines']).strftime('%Y-%m-%d')
+                git['PyExpLabSys'] = datetime.datetime.fromtimestamp(
+                    gittime['PyExpLabSys']
+                ).strftime('%Y-%m-%d')
+                git['machines'] = datetime.datetime.fromtimestamp(
+                    gittime['machines']
+                ).strftime('%Y-%m-%d')
             except TypeError:
                 # Legacy purposes only
-                git['PyExpLabSys'] = datetime.datetime.fromtimestamp(gittime).strftime('%Y-%m-%d')
+                git['PyExpLabSys'] = datetime.datetime.fromtimestamp(gittime).strftime(
+                    '%Y-%m-%d'
+                )
                 git['machines'] = 'None'
         except TypeError:
             git['PyExpLabSys'] = 'None'
             git['machines'] = 'None'
-        except  UnboundLocalError:
+        except UnboundLocalError:
             git['PyExpLabSys'] = ''
             git['machines'] = ''
         return_value['git_pyexplabsys'] = git['PyExpLabSys']
         return_value['git_machines'] = git['machines']
 
     return return_value
+
 
 class CheckHost(threading.Thread):
     """ Perfom the actual check """
@@ -220,13 +277,16 @@ class CheckHost(threading.Thread):
         self.results = results_queue
 
     def run(self):
-        suffixes = ["", ".fys.clients.local"]
+        suffixes = [""] + SUFFIX_LIST
         while not self.hosts.empty():
             host = self.hosts.get_nowait()
             try:
                 attr = json.loads(host[5])
                 print(host, attr)
-            except (TypeError, json.decoder.JSONDecodeError): # Happens if attr is empty
+            except (
+                TypeError,
+                json.decoder.JSONDecodeError,
+            ):  # Happens if attr is empty
                 # json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0) if attr = ''
                 print(host, 'typeerror')
                 attr = {}
@@ -237,6 +297,8 @@ class CheckHost(threading.Thread):
                 attr['apt_up'] = ''
                 attr['location'] = host[3]
                 attr['purpose'] = host[4]
+                attr['os_version'] = ''
+                attr['last_accessed'] = ''
             for suffix in suffixes:
                 host_is_up = host_status(host[1] + suffix, host[2])
                 if host_is_up:
@@ -247,7 +309,9 @@ class CheckHost(threading.Thread):
 
             if host_is_up:
                 username, password = credentials.get_user_and_password(hostname)
-                uptime_val = uptime(hostname, host[2], username=username, password=password)
+                uptime_val = uptime(
+                    hostname, host[2], username=username, password=password
+                )
             else:
                 uptime_val = {}
                 uptime_val['up'] = ''
@@ -255,16 +319,24 @@ class CheckHost(threading.Thread):
                 try:
                     uptime_val['git_pyexplabsys'] = attr['git_pyexplabsys']
                     uptime_val['git_machines'] = attr['git_machines']
-                except KeyError: # Legacy support
+                except KeyError:  # Legacy support
                     uptime_val['git_pyexplabsys'] = attr['git']
                     uptime_val['git_machines'] = 'None'
                 uptime_val['host_temperature'] = ''
                 uptime_val['model'] = attr['model']
                 try:
+                    uptime_val['os_version'] = attr['os_version']
+                except KeyError:
+                    uptime_val['os_version'] = ''
+                try:
                     uptime_val['apt_up'] = attr['apt_up']
                 except KeyError:
                     uptime_val['apt_up'] = ''
                 uptime_val['python_version'] = attr['python_version']
+                try:
+                    uptime_val['last_accessed'] = attr['last_accessed']
+                except KeyError:
+                    uptime_val['last_accessed'] = ''
 
             uptime_val['db_id'] = host[0]
             uptime_val['hostname'] = host[1]
@@ -293,6 +365,7 @@ class CheckHost(threading.Thread):
 
             self.hosts.task_done()
 
+
 def main():
     """ Main function """
     hosts = queue.Queue()
@@ -306,7 +379,7 @@ def main():
     cursor = database.cursor()
     database.autocommit(True)
 
-    query = 'select id, host, port, location, purpose, attr from host_checker';
+    query = 'select id, host, port, location, purpose, attr from host_checker'
     cursor.execute(query)
     results = cursor.fetchall()
 
@@ -316,7 +389,7 @@ def main():
     results = queue.Queue()
 
     host_checkers = {}
-    for i in range(0, 5): # number threads, high number will complete faster
+    for i in range(0, 5):  # number threads, high number will complete faster
         host_checkers[i] = CheckHost(hosts, results)
         host_checkers[i].start()
     hosts.join()
@@ -324,11 +397,16 @@ def main():
     while not results.empty():
         host = results.get()
         LOGGER.debug('Value of host: ' + str(host))
-        query = ("update host_checker set time=now(), attr = '" +
-                 json.dumps(host) + "' where id = " + str(host['db_id']))
+        query = (
+            "update host_checker set time=now(), attr = '"
+            + json.dumps(host)
+            + "' where id = "
+            + str(host['db_id'])
+        )
         LOGGER.debug(query)
         cursor.execute(query)
     database.close()
+
 
 if __name__ == "__main__":
     minutes_failed = 0
